abd043d7387b4a04c821ac85b0e9f87558fd93d0
Guilherme Figueiroa <guilherme.mfigueiroa@gmail.com>
2016-03-15 22:47:08 -0300

    Adicionada teste para a rodada morte súbita

---
+++ src/test/MorteSubitaTeste.java
@@ -0,0 +1,36 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.coursera.figueiroa.mecanicas.FabricaMecanicaDoJogo;
+import org.coursera.figueiroa.mecanicas.MecanicaDoJogo;
+import org.junit.Before;
+import org.junit.Test;
+
+public class MorteSubitaTeste {
+
+	MecanicaDoJogo mecanica;
+	
+	@Before
+	public void setUp() throws Exception {
+		mecanica = FabricaMecanicaDoJogo.getMorteSubita();
+	}
+
+	@Test
+	public void testeRodada() {
+		assertFalse(mecanica.jogoFinalizado());
+		assertEquals(0, mecanica.getPontuacao());
+		
+		assertEquals("EMREHLIUG", mecanica.novaRodada("GUILHERME"));
+		assertTrue(mecanica.adivinharPalavra("GuIlHeRmE"));
+		assertFalse(mecanica.jogoFinalizado());
+		assertEquals(10, mecanica.getPontuacao());
+		
+		assertEquals("EMREHLIUG", mecanica.novaRodada("GUILHERME"));
+		assertFalse(mecanica.adivinharPalavra("OUTRAPALAVRA"));
+		assertTrue(mecanica.jogoFinalizado());
+		
+		assertEquals(10, mecanica.getPontuacao());
+	}
+
+}
